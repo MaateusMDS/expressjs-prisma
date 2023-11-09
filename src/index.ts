1e33b8334ab8a5c6a2a2e1410a7652c5f07eae1e
MaateusMDS <maateusmania@gmail.com>
2023-11-09 15:54:02 -0300

    feat: todo remove and added interceptor

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 import { PrismaClient } from "@prisma/client";
-import express from "express";
+import express, { Request, Response } from "express";
 import ProductController from "./controllers/ProductController";
 
 const prisma = new PrismaClient();
@@ -7,57 +7,19 @@ const prisma = new PrismaClient();
 const app = express();
 const port = process.env.PORT || 3000;
 
+app.use((req: Request, res: Response, next: any) => {
+  const authHeader = req.headers.authorization || '';
+  if (authHeader.split(" ")[1] === "123456") {
+      next();
+  } else {
+      res.status(401).send("Unauthorized");
+      res.end();
+  }
+});
+
 app.use(express.json());
 app.use(express.raw({ type: "application/vnd.custom-type" }));
 app.use(express.text({ type: "text/html" }));
-
-app.get("/todos", async (req, res) => {
-  const todos = await prisma.todo.findMany({
-    orderBy: { createdAt: "desc" },
-  });
-
-  res.json(todos);
-});
-
-app.post("/todos", async (req, res) => {
-  const todo = await prisma.todo.create({
-    data: {
-      completed: false,
-      createdAt: new Date(),
-      text: req.body.text ?? "Empty todo",
-    },
-  });
-
-  return res.json(todo);
-});
-
-app.get("/todos/:id", async (req, res) => {
-  const id = req.params.id;
-  const todo = await prisma.todo.findUnique({
-    where: { id },
-  });
-
-  return res.json(todo);
-});
-
-app.put("/todos/:id", async (req, res) => {
-  const id = req.params.id;
-  const todo = await prisma.todo.update({
-    where: { id },
-    data: req.body,
-  });
-
-  return res.json(todo);
-});
-
-app.delete("/todos/:id", async (req, res) => {
-  const id = req.params.id;
-  await prisma.todo.delete({
-    where: { id },
-  });
-
-  return res.send({ status: "ok" });
-});
 
 app.get("/products", ProductController.getAllProducts);
 
